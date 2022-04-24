a7947bee50fb08de99035c925ea87906d16d22a2
3eca <40839411+3eca@users.noreply.github.com>
2022-04-24 19:35:03 +0300

    Update bot_config.py

--- bot_config.py
+++ bot_config.py
@@ -2,9 +2,9 @@
 # -*- coding: utf-8 -*-
 
 ADMIN_ID = ''
-CHANNEL_ID = '@testholyNash'
-TOKEN = '5297067815:AAH2gi75KwkyfHPhCK1FIZ1skiXxdbEjXqs'
-DIR_PATH = 'ImageHotGirls/'
+CHANNEL_ID = ''
+TOKEN = ''
+DIR_PATH = ''
 
 
 if __name__ == '__main__':
